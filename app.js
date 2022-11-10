f2a409963196f2432d981a5f0ba14ec42c19e667
kyerz <mikael.verdu@gmail.com>
2022-11-10 17:34:38 +0100

    add import module

--- app.js
+++ app.js
@@ -1,23 +1,13 @@
+import jsonPaths from './pathlist.json' assert { type: 'json' };
+
+const pathList = jsonPaths.pathlist;
 const board = document.querySelector('.board');
 const spanTimer = document.querySelector('.timer');
 const ctnTimer = document.querySelector('.timer-ctn');
 const ctnRestart = document.querySelector('.restart-ctn');
 const btnRestart = document.querySelector('#restart');
 const hitScore = document.querySelector('.hitscore');
-const pathList = [
-  '1.jpg',
-  '2.jpg',
-  '3.jpg',
-  '4.jpg',
-  '5.jpg',
-  '6.jpg',
-  '7.jpg',
-  '8.jpg',
-  '9.jpg',
-  '10.jpg',
-  '11.jpg',
-  '12.jpg',
-];
+
 const storage = localStorage;
 const shufflePaths = [];
 const selectedPaths = [];
